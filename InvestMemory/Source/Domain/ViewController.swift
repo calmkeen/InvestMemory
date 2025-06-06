cbe063398ddb2898d7db0b1212e311ad57981a41
Younsung <78361650+calmkeen@users.noreply.github.com>
2025-06-06 17:38:35 +0900

    New: make new project

---
+++ InvestMemory/Source/Domain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InvestMemory
+//
+//  Created by calmkeen on 6/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
